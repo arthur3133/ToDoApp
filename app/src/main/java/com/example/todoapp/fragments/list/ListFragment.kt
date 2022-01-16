6ff1533ec734fd4af44196ccf7453e6cdd4a3d1e
arthur3133 <alanprajapati8624@gmail.com>
2022-01-16 10:36:41 +0530

    Create List Fragment Menu

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todoapp.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
 import kotlinx.android.synthetic.main.fragment_list.*
@@ -26,6 +24,13 @@ class ListFragment : Fragment() {
         view.list_layout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        // Set Menu
+        setHasOptionsMenu(true)
         return view;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
